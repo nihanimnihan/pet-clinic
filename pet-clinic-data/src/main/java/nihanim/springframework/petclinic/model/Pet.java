614f5705a493eda52be9cdded1be8d91a4b2b12f
nihanimnihan <nihanimnihan@gmail.com>
2018-10-19 14:43:40 +0300

    Creating Pet Type, Pet and Visit Entities Closes #26.

--- pet-clinic-data/src/main/java/nihanim/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/nihanim/springframework/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package nihanim.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
 
     private Owner owner;
     private PetType petType;
