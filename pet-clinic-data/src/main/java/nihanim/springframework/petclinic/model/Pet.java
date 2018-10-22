d771987de95e87711c3295a1fb791eadcb850476
nihanimnihan <nihanimnihan@gmail.com>
2018-10-22 23:39:04 +0300

    Creating Visit Entity. Closes #37.

--- pet-clinic-data/src/main/java/nihanim/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/nihanim/springframework/petclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package nihanim.springframework.petclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity{
@@ -19,6 +18,8 @@ public class Pet extends BaseEntity{
     private PetType petType;
     @Column
     private LocalDate birthDate;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +51,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
